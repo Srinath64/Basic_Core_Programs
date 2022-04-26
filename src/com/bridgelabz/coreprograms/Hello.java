19efac281508e9d72f8c64f2ae7d121577a3c08f
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-04-26 14:37:35 +0530

    Added : Modified Hello Program

--- src/com/bridgelabz/coreprograms/Hello.java
+++ src/com/bridgelabz/coreprograms/Hello.java
@@ -2,6 +2,6 @@ package com.bridgelabz.coreprograms;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Java Program");
     }
 }
